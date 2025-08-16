8df6f6a09619e6678dc1500d9e7689c7a3caf79d
NaClLuo <159599950+NaClLuo@users.noreply.github.com>
2025-08-16 16:33:59 +0800

    Delete drivers/net/ethernet directory

--- drivers/net/ethernet/mediatek/mtk_hnat/hnat_nf_hook.c
+++
@@ -1,6 +0,0 @@
-// Commented out pr_info log lines in foe_clear_entry function
-
-void foe_clear_entry(...) {
-    // pr_info("...\n"); // Original log line
-    // Additional code...
-}
--- drivers/net/ethernet/raeth/mtk_hnat/hnat_nf_hook.c
+++
@@ -1,9 +0,0 @@
-// Original content of the file goes here
-
-void foe_clear_entry(...) {
-    // Commented out the following lines
-    // pr_info("State: ...");
-    // pr_info("Deleting old entry...");
-}
-
-// Rest of the file content continues...
