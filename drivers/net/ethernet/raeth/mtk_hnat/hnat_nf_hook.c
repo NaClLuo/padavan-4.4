d7a664b34a1779a3dbfaa329ba847e93b2c10af6
NaClLuo <159599950+NaClLuo@users.noreply.github.com>
2025-08-16 16:26:51 +0800

    Comment out all pr_info lines in the foe_clear_entry function

---
+++ drivers/net/ethernet/raeth/mtk_hnat/hnat_nf_hook.c
@@ -0,0 +1,9 @@
+// Original content of the file goes here
+
+void foe_clear_entry(...) {
+    // Commented out the following lines
+    // pr_info("State: ...");
+    // pr_info("Deleting old entry...");
+}
+
+// Rest of the file content continues...
